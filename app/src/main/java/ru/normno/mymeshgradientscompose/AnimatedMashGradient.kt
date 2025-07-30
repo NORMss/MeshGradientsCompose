299354c51c66da04d3c3bca92dc12df4e5dbb06c
NORMss <serega.b301@gmail.com>
2025-07-30 23:59:13 +0700

    feat: add AnimatedMashGradient modifier

--- app/src/main/java/ru/normno/mymeshgradientscompose/AnimatedMashGradient.kt
+++ app/src/main/java/ru/normno/mymeshgradientscompose/AnimatedMashGradient.kt
@@ -3,18 +3,11 @@ package ru.normno.mymeshgradientscompose
 import android.graphics.RuntimeShader
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ShaderBrush
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.unit.dp
 import org.intellij.lang.annotations.Language
 
 @Language("AGSL")
@@ -22,38 +15,48 @@ private val CUSTOM_SHADER = """
     uniform float2 resolution;
     layout(color) uniform half4 color;
     layout(color) uniform half4 color2;
-
+    uniform float2 point;
+    
     half4 main(in float2 fragCoord) {
-        float2 uv = fragCoord/resolution.xy;
-
-        float mixValue = distance(uv, vec2(0, 1));
+        float2 uv = fragCoord / resolution.xy;
+    
+        float mixValue = distance(uv, point);
         return mix(color, color2, mixValue);
     }
 """.trimIndent()
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-fun Modifier.basicMashGradient(
-    firstColor: Color = Color.Unspecified,
-    secondColor: Color = Color.Unspecified,
+fun Modifier.animatedMashGradient(
+    firstColor: Color,
+    secondColor: Color,
+    point: Offset,
 ) = drawWithCache {
     val shader = RuntimeShader(CUSTOM_SHADER)
     val shaderBrush = ShaderBrush(shader)
+
     shader.setFloatUniform("resolution", size.width, size.height)
+
     onDrawBehind {
         shader.setColorUniform(
             "color",
             android.graphics.Color.valueOf(
-                firstColor.red, firstColor.green, firstColor.blue, firstColor.alpha,
+                firstColor.red,
+                firstColor.green,
+                firstColor.blue,
+                firstColor.alpha
             )
         )
         shader.setColorUniform(
             "color2",
             android.graphics.Color.valueOf(
-                secondColor.red, secondColor.green, secondColor.blue, secondColor.alpha,
+                secondColor.red,
+                secondColor.green,
+                secondColor.blue,
+                secondColor.alpha
             )
         )
-        drawRect(
-            brush = shaderBrush,
-        )
+        shader.setFloatUniform("point", point.x, point.y)
+
+        drawRect(brush = shaderBrush)
     }
 }
--- app/src/main/java/ru/normno/mymeshgradientscompose/MainActivity.kt
+++ app/src/main/java/ru/normno/mymeshgradientscompose/MainActivity.kt
@@ -4,21 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.normno.mymeshgradientscompose.ui.theme.MyMeshGradientsComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +39,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyMeshGradientsComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GradientBox(
+                    Column(
                         modifier = Modifier
-                            .padding(innerPadding)
-                    )
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        GradientBox()
+                        Spacer(modifier = Modifier.height(16.dp))
+                        GradientBoxWithAnimation()
+                    }
                 }
             }
         }
@@ -44,21 +63,68 @@ fun GradientBox(
 ) {
     Box(
         modifier = modifier
-            .fillMaxSize(
+            .fillMaxWidth(
                 0.6f,
             )
             .aspectRatio(1f)
-            .basicMashGradient(
-                firstColor = Color.Blue,
-                secondColor = Color.Red,
-            )
             .clip(
                 RoundedCornerShape(
                     16.dp,
                 )
+            )
+            .basicMashGradient(
+                firstColor = Color.Cyan,
+                secondColor = Color.DarkGray,
             ),
         contentAlignment = Alignment.Center,
     ) {
+        Text(
+            text = "Test Mash Gradient",
+        )
+    }
+}
 
+@Preview
+@Composable
+fun GradientBoxWithAnimation(
+    modifier: Modifier = Modifier,
+) {
+    val animatedX = remember { Animatable(0f) }
+    val animatedY = remember { Animatable(1f) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            animatedX.animateTo(
+                targetValue = Random.nextFloat(),
+                animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
+            )
+            animatedY.animateTo(
+                targetValue = Random.nextFloat(),
+                animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
+            )
+        }
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth(
+                0.6f,
+            )
+            .aspectRatio(1f)
+            .clip(
+                RoundedCornerShape(
+                    16.dp,
+                )
+            )
+            .animatedMashGradient(
+                firstColor = Color.Cyan,
+                secondColor = Color.DarkGray,
+                point = Offset(animatedX.value, animatedY.value)
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "Test Animated Mash Gradient",
+        )
     }
 }
